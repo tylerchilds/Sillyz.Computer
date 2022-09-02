349e4dbc651e07cb63368a4f74a1f875108680f4
Tyler Childs <email@tychi.me>
2022-09-02 10:09:20 -0700

    gate native client with environment variable

--- client.js
+++ client.js
@@ -1,6 +1,17 @@
-import { Webview } from "https://deno.land/x/webview/mod.ts";
+const isNative = Deno.env.get("NATIVE")
 
-const webview = new Webview();
+async function nativeClient() {
+  const { Webview } = await import("https://deno.land/x/webview/mod.ts")
+  const webview = new Webview()
 
-webview.navigate("http://127.0.0.1:8000/404");
-webview.run();
+  webview.navigate("http://127.0.0.1:8000")
+  webview.run()
+}
+
+async function browserClient() {
+  const { open } = await import("https://deno.land/x/opener/mod.ts")
+  await open('http://127.0.0.1:8000')
+}
+
+console.log({ isNative })
+isNative ? nativeClient() : browserClient()
