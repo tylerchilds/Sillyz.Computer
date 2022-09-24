bfa3cf53e44f3f8b874bac4e108e76cf3f8cf5aa
Tyler Childs <email@tychi.me>
2022-09-24 13:20:41 -0700

    fix octave count

--- packages/streams/instrument.js
+++ packages/streams/instrument.js
@@ -81,7 +81,7 @@ const colors = {
   "   xx": [3, 2]
 }
 
-export const octaves = [2, 3, 4, 5, 6, -2, -1, 0, 1]
+export const octaves = [2, 3, 4, 5, 6, -2, -1, 0]
 
 let baseOctave = 0
 const eventMap = {
