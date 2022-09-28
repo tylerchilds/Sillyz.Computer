7881a53905040516eec43846c50289cdc4bdc00a
Tyler Childs <email@tychi.me>
2022-09-28 13:40:09 -0700

    add slideshow for synchronized style sheets

--- packages/ui/drawer.js
+++ packages/ui/drawer.js
@@ -103,7 +103,7 @@ $.style(`
       position: fixed;
       top: 0;
       bottom: 0;
-      background: rgba(0,0,0,.8);
+      background: rgba(255,255,255,.75);
       z-index: 900;
       overflow-y: auto;
       max-width: 100%;
