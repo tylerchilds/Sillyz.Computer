631ac79aa82f6eab633a59415b29a35dcd2a0de5
Tyler Childs <email@tychi.me>
2022-11-01 12:08:07 -0700

    cleaner impl

--- routes/edge/[...index].js
+++ routes/edge/[...index].js
@@ -1,17 +1,16 @@
 export const handler = async (request, context) => {
   const pathname = context.params.index
+      console.log(pathname)
 
-  const edgeHandler = await fetch(`https://1998.social/edge/${pathname}.js`)
+  const { handler } = await fetch(`https://1998.social/edge/${pathname}.js`)
     .then((res) => res.json())
-    .then(async (data) => {
+    .then((data) => {
+      console.log(data)
       if(!data || !data.func) return 'did we edgespect that??'
       const b64moduleData = "data:text/javascript;base64," + btoa(data.func);
 
-      const { handler } = await import(b64moduleData);
-      console.log(handler)
-      return handler
+      return import(b64moduleData);
     })
 
-  console.log(edgeHandler)
-  return await edgeHandler(request, context)
+  return await handler(request, context)
 }
