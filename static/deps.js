e64f90dd05930e73d94232ea89b7fb46096b5e67
Tyler Childs <email@tychi.me>
2023-01-16 13:33:30 -0800

    bump tag to latest

--- static/deps.js
+++ static/deps.js
@@ -1,4 +1,4 @@
-import tag, { listen, on, signal, read, write } from 'https://deno.land/x/tag@v0.3.2/mod.js';
+import tag, { listen, on, signal, read, write } from 'https://deno.land/x/tag@v0.3.4/mod.js';
 import * as focusTrap from 'https://esm.sh/focus-trap';
 import Color from "https://esm.sh/colorjs.io@0.4.0";
 import Quill from 'https://esm.sh/quill@1.3.7'
